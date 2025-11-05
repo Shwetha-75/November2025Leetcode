61d09e414bd1885f800e8c6422eccdc7fc925a44
Shwetha K <shweetha.sweety17@gmail.com>
2025-11-05 20:19:39 +0530

    25-11-05 :: 2025 Nov 05 : Shwetha K : 3321 Find X Sum of All K Long Subarrays II

---
+++ 3321FindXSumofAllKLongSubarraysII/3321FindXSumofAllKLongSubarraysII.py
@@ -0,0 +1,85 @@
+from collections import Counter
+class Solution:
+    def findXSum(self, nums: list[int], k: int, x: int) -> list[int]:
+        ans,n=[],len(nums)
+        for i in range(n-k+1):
+            window=nums[i:i+k]
+            freq_map=Counter(window)
+            top=sorted(freq_map.items(),key= lambda x:(x[1],x[0]),reverse=True)[:x]
+            top=set(v for v,_ in top)
+            ans.append(sum(num for num in window if num in top))
+        return ans 
+
+from collections import defaultdict
+from sortedcontainers import SortedList
+class Helper:
+    def __init__(self, x):
+        self.x = x
+        self.result = 0
+        self.large = SortedList()
+        self.small = SortedList()
+        self.occ = defaultdict(int)
+
+    def insert(self, num):
+        if self.occ[num] > 0:
+            self.internal_remove((self.occ[num], num))
+        self.occ[num] += 1
+        self.internal_insert((self.occ[num], num))
+
+    def remove(self, num):
+        self.internal_remove((self.occ[num], num))
+        self.occ[num] -= 1
+        if self.occ[num] > 0:
+            self.internal_insert((self.occ[num], num))
+
+    def get(self):
+        return self.result
+
+    def internal_insert(self, p):
+        if len(self.large) < self.x or p > self.large[0]:
+            self.result += p[0] * p[1]
+            self.large.add(p)
+            if len(self.large) > self.x:
+                to_remove = self.large[0]
+                self.result -= to_remove[0] * to_remove[1]
+                self.large.remove(to_remove)
+                self.small.add(to_remove)
+        else:
+            self.small.add(p)
+
+    def internal_remove(self, p):
+        if p >= self.large[0]:
+            self.result -= p[0] * p[1]
+            self.large.remove(p)
+            if self.small:
+                to_add = self.small[-1]
+                self.result += to_add[0] * to_add[1]
+                self.small.remove(to_add)
+                self.large.add(to_add)
+        else:
+            self.small.remove(p)
+
+
+class Solution:
+    def findXSum(self, nums, k, x):
+        helper = Helper(x)
+        ans = []
+
+        for i in range(len(nums)):
+            helper.insert(nums[i])
+            if i >= k:
+                helper.remove(nums[i - k])
+            if i >= k - 1:
+                ans.append(helper.get())
+
+        return ans
+class TestApp:
+    def test_case_one(self):
+        assert Solution().findXSum([1,1,2,2,3,4,2,3],6,2)==[6,10,12]
+    def test_case_two(self):
+        assert Solution().findXSum([3,8,7,8,7,5],2,2)==[11,15,15,15,12]
+        
+               
+                    
+            
+            
